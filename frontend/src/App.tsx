e455da5739552e3276b9e43f53d6985d18e0bc25
mandashilpa26 <mandashilpa26@gmail.com>
2019-05-15 18:12:27 +0100

    UI for shelter app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, CardColumns } from 'react-bootstrap';
+import PetCard from './PetCard';
+
+var favoritePets = [
+  { id: "1", name: "Berty", description: "Has a good nose for choclates" },
+  { id: "2", name: "Argo", description: "Batman (of the dog world)" },
+  { id: "3", name: "Fred", description: "Only sleeps and eats..!" },
+];
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+    <Row>
+    <Col>
+      <CardColumns>
+      {
+        favoritePets.map((pet) => <PetCard key={pet.id} pet={pet} />)
+      }
+      </CardColumns>
+    </Col>
+    </Row>
+  </Container>
   );
 }
 
